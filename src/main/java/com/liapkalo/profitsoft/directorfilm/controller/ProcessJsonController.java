3bfeacb2946f2805964343b9ddb74bf12f439159
Julia <julialyapkalo@gmail.com>
2024-04-12 16:44:19 +0300

    added controller

---
+++ src/main/java/com/liapkalo/profitsoft/directorfilm/controller/ProcessJsonController.java
@@ -0,0 +1,30 @@
+package com.liapkalo.profitsoft.directorfilm.controller;
+
+import com.liapkalo.profitsoft.directorfilm.service.GenerateXMLStatisticService;
+import com.liapkalo.profitsoft.directorfilm.service.ParseJsonService;
+import com.liapkalo.profitsoft.directorfilm.service.WriteXMLToFileService;
+import com.liapkalo.profitsoft.directorfilm.service.impl.GenerateXMLStatisticServiceImpl;
+import com.liapkalo.profitsoft.directorfilm.service.impl.ParseJsonServiceImpl;
+import com.liapkalo.profitsoft.directorfilm.service.impl.WriteXMLToFileServiceImpl;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ProcessJsonController {
+
+    static ParseJsonService parseJsonService = new ParseJsonServiceImpl();
+    static GenerateXMLStatisticService generateXmlStatistics = new GenerateXMLStatisticServiceImpl();
+    static WriteXMLToFileService writeXMLToFileService = new WriteXMLToFileServiceImpl();
+
+    public static void processJson(String path, String attribute) {
+        Map<String, Integer> attributeCounts = new HashMap<>();
+
+        parseJsonService.parseFiles(path, attribute, attributeCounts);
+        writeXMLToFileService.writeXmlToFile(attribute, generateXmlStatistics.generateXmlStatistics(attributeCounts));
+    }
+}
